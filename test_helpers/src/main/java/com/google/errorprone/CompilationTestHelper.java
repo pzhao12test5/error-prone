c69d484b9e37776dfe28d0519931d2430f2158f0
DrRacket <zpy1995@gmail.com>
2017-12-09 11:58:37 +0000

    bb7973e8002b replay gib_eks

--- test_helpers/src/main/java/com/google/errorprone/CompilationTestHelper.java
+++ test_helpers/src/main/java/com/google/errorprone/CompilationTestHelper.java
@@ -57,7 +57,8 @@ public class CompilationTestHelper {
           "UTF-8",
           // print stack traces for completion failures
           "-XDdev",
-          "-XDsave-parameter-names");
+          "-parameters",
+          "-XDcompilePolicy=simple");
 
   private final DiagnosticTestHelper diagnosticHelper;
   private final BaseErrorProneCompiler compiler;
